73193755340f65db8eda37990d928800b1c7ecb8
Busayo O <44685320+beethedev@users.noreply.github.com>
2021-07-14 15:41:34 -0700

    update app

---
+++ Project7/Petition.swift
@@ -0,0 +1,14 @@
+//
+//  Petition.swift
+//  Project7
+//
+//  Created by Oluwabusayo Olorunnipa on 7/13/21.
+//
+
+import Foundation
+
+struct Petition: Codable {
+    var title: String
+    var body: String
+    var signatureCount: Int
+}
---
+++ Project7/Petitions.swift
@@ -0,0 +1,12 @@
+//
+//  Petitions.swift
+//  Project7
+//
+//  Created by Oluwabusayo Olorunnipa on 7/13/21.
+//
+
+import Foundation
+
+struct Petitions: Codable {
+    var results: [Petition]
+}
